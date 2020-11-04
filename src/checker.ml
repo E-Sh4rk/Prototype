1362afd28fcae3cd10693d189eca1e32c890c865
Mickael Laurent <esh4rk@gmail.com>
2020-11-04 02:07:22 +0100

    abstract and optimize the Env type

--- src/checker.ml
+++ src/checker.ml
@@ -3,14 +3,19 @@ open Variable
 open Nf_ast
 open Types_additions
 
-type env = typ VarMap.t
-let empty_env = VarMap.empty
-
-let is_bottom env =
-    let is_bottom (_,t) = is_empty t in
-    List.exists is_bottom (VarMap.bindings env)
-
-let add_to_env v t env = VarMap.add v t env
+module Env = struct
+  type t = typ VarMap.t * VarSet.t (* bottom vars *)
+  let empty = (VarMap.empty, VarSet.empty)
+  let is_bottom (_, bvs) = VarSet.is_empty bvs |> not
+  let add v t (env, bvs) =
+    let bvs =
+      if is_empty t
+      then VarSet.add v bvs
+      else VarSet.remove v bvs
+    in
+    (VarMap.add v t env, bvs)
+  let find v (env, _) = VarMap.find v env
+end
 
 exception Ill_typed of Position.t list * string
 
@@ -22,22 +27,22 @@ let rec typeof tenv env e =
     (* Var & const *)
     | Const (Atom str) -> get_type_or_atom tenv str
     | Const c -> Ast.const_to_typ c
-    | Var v -> VarMap.find v env
+    | Var v -> Env.find v env
     | Debug (str, v) ->
-      let res = VarMap.find v env in
+      let res = Env.find v env in
       Format.printf "%s (typeof): " str ; Utils.print_type res ;
       res
     (* Projections & Pairs *)
     | Projection (Fst, v) ->
-      let t = VarMap.find v env in
+      let t = Env.find v env in
       if subtype t pair_any then pi1 t
       else raise (Ill_typed (pos, "Fst can only be applied to a pair."))
     | Projection (Snd, v) ->
-      let t = VarMap.find v env in
+      let t = Env.find v env in
       if subtype t pair_any then pi2 t
       else raise (Ill_typed (pos, "Snd can only be applied to a pair."))
     | Projection (Field str, v) ->
-      let t = VarMap.find v env in
+      let t = Env.find v env in
       if subtype t record_any
       then
         try get_field t str
@@ -45,24 +50,24 @@ let rec typeof tenv env e =
         raise (Ill_typed (pos, Printf.sprintf "The record does not surely contains the field %s." str))
       else raise (Ill_typed (pos, "Field projection can only be applied to a record."))
     | Pair (x1, x2) ->
-      let t1 = VarMap.find x1 env in
-      let t2 = VarMap.find x2 env in
+      let t1 = Env.find x1 env in
+      let t2 = Env.find x2 env in
       mk_times (cons t1) (cons t2)
     | RecordUpdate (x1, str, x2) ->
-      let t1 = VarMap.find x1 env in
+      let t1 = Env.find x1 env in
       if subtype t1 record_any
       then begin
         match x2 with
         | None -> remove_field t1 str
         | Some x2 ->
-          let t2 = VarMap.find x2 env in
+          let t2 = Env.find x2 env in
           let t' = mk_record false [str, cons t2] in
           merge_records t1 t'
       end else raise (Ill_typed (pos, "Field assignation can only be applied to a record."))
     (* App & Case *)
     | App (x1, x2) ->
-      let t1 = VarMap.find x1 env in
-      let t2 = VarMap.find x2 env in
+      let t1 = Env.find x1 env in
+      let t2 = Env.find x2 env in
       let dom_t1 = domain t1 in
       if subtype t2 dom_t1
       then apply t1 t2
@@ -72,9 +77,9 @@ let rec typeof tenv env e =
           (string_of_type dom_t1) (string_of_type t2) in
         raise (Ill_typed (pos, error))
     | Ite (v,t,e1,e2) ->
-      let vt = VarMap.find v env in
-      let env1 = VarMap.add v (cap vt t) env in
-      let env2 = VarMap.add v (cap vt (neg t)) env in
+      let vt = Env.find v env in
+      let env1 = Env.add v (cap vt t) env in
+      let env2 = Env.add v (cap vt (neg t)) env in
       (* TODO: some marking in order to detect unreachable code *)
       (*let logs1 = get_logs_expr e1 in
       let logs2 = get_logs_expr e2 in
@@ -99,7 +104,7 @@ let rec typeof tenv env e =
       |> conj
     | Lambda _ -> failwith "Only abstractions with typed domain are supported for now."
   and aux_e pos env e =
-    if is_bottom env
+    if Env.is_bottom env
     then empty
     else match e with 
     | Atomic a -> aux_a pos env a
@@ -117,7 +122,7 @@ and split_and_refine tenv env e x initial_t refine_env_cont =
   let rec aux env t =
     let envs = refine_env_cont env t in
     let treat_env (t, env) =
-      let env = VarMap.add x t env in
+      let env = Env.add x t env in
       match candidates_partition tenv env e x t with
       | [] -> assert false
       | [t] -> [t, env]
@@ -153,26 +158,26 @@ and refine ~backward tenv env e t =
     begin match a with
     (* Var & const *)
     | Const _ -> [t, env]
-    | Var v -> [t, VarMap.add v t env]
-    | Debug (_, v) -> [t, VarMap.add v t env]
+    | Var v -> [t, Env.add v t env]
+    | Debug (_, v) -> [t, Env.add v t env]
     (* Projections & Pairs *)
     | Projection (Fst, v) ->
-      let tv = VarMap.find v env in
-      [t, VarMap.add v (cap tv (mk_times (cons t) any_node)) env]
+      let tv = Env.find v env in
+      [t, Env.add v (cap tv (mk_times (cons t) any_node)) env]
     | Projection (Snd, v) ->
-      let tv = VarMap.find v env in
-      [t, VarMap.add v (cap tv (mk_times any_node (cons t))) env]
+      let tv = Env.find v env in
+      [t, Env.add v (cap tv (mk_times any_node (cons t))) env]
     | Projection (Field str, v) ->
-      let tv = VarMap.find v env in
-      [t, VarMap.add v (cap tv (mk_record true [str, cons t])) env]
+      let tv = Env.find v env in
+      [t, Env.add v (cap tv (mk_record true [str, cons t])) env]
     | Pair (x1, x2) ->
       split_pair t
       |> List.map (
         fun (t1, t2) ->
           let env =
             if Variable.equals x1 x2
-            then VarMap.add x1 (cap t1 t2) env
-            else VarMap.add x1 t1 (VarMap.add x2 t2 env)
+            then Env.add x1 (cap t1 t2) env
+            else Env.add x1 t1 (Env.add x2 t2 env)
           in
           (mk_times (cons t1) (cons t2), env)
       )
@@ -180,8 +185,8 @@ and refine ~backward tenv env e t =
       failwith "Refinement of records not implemented yet."
     (* App & Case *)
     | App (x1, x2) ->
-      let t1 = VarMap.find x1 env in
-      let t2 = VarMap.find x2 env in
+      let t1 = Env.find x1 env in
+      let t2 = Env.find x2 env in
       (if backward then square t1 t else triangle t1 t)
       |> List.map (
         fun (t1', t2') ->
@@ -189,15 +194,15 @@ and refine ~backward tenv env e t =
           let t2 = cap t2 t2' in
           let env =
             if Variable.equals x1 x2
-            then VarMap.add x1 (cap t1 t2) env
-            else VarMap.add x1 t1 (VarMap.add x2 t2 env)
+            then Env.add x1 (cap t1 t2) env
+            else Env.add x1 t1 (Env.add x2 t2 env)
           in
           (apply t1 t2, env)
       )
     | Ite (v,t,e1,e2) ->
-      let vt = VarMap.find v env in
-      let env1 = VarMap.add v (cap vt t) env in
-      let env2 = VarMap.add v (cap vt (neg t)) env in
+      let vt = Env.find v env in
+      let env1 = Env.add v (cap vt t) env in
+      let env2 = Env.add v (cap vt (neg t)) env in
       (aux_e env1 e1 t)@(aux_e env2 e2 t)
     (* Abstractions *)
     | Lambda (Ast.ADomain s, x, e) ->
--- src/checker.mli
+++ src/checker.mli
@@ -3,13 +3,16 @@ open Types_additions
 open Cduce
 open Variable
 
-type env
-val empty_env : env
-val is_bottom : env -> bool
-val add_to_env : Variable.t -> typ -> env -> env
+module Env : sig
+  type t
+  val empty : t
+  val is_bottom : t -> bool
+  val add : Variable.t -> typ -> t -> t
+  val find : Variable.t -> t -> typ
+end
 
 exception Ill_typed of Position.t list * string
 
-val typeof : type_env -> env -> e -> typ
-val candidates : type_env -> env -> e -> Variable.t -> typ list
-val refine : backward:bool -> type_env -> env -> e -> typ -> (typ * env) list
+val typeof : type_env -> Env.t -> e -> typ
+val candidates : type_env -> Env.t -> e -> Variable.t -> typ list
+val refine : backward:bool -> type_env -> Env.t -> e -> typ -> (typ * Env.t) list

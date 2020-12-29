1820b6eee1a1cfaa5bcb6b9ec524331b07817660
Mickael Laurent <esh4rk@gmail.com>
2020-12-29 19:53:39 +0100

    update refine function

--- src/checker.ml
+++ src/checker.ml
@@ -45,73 +45,50 @@ and typeof (*tenv env env'*) _ _ _ e =
   | Let (v, a, e) -> failwith "TODO"*)
   | _ -> failwith "TODO"
 
-let rec refine_a _ ~backward _ env a t = (* TODO: Update with the new system... *)
-  if Env.is_bottom env then []
-  else begin
-    match a with
-    | Const c when backward ->
-      if  disjoint (Ast.const_to_typ c) t then [] else [Env.empty]
-    | Const c ->
-      if  subtype (Ast.const_to_typ c) t then [Env.empty] else []
-    | Var v -> [Env.singleton v t]
-    | Debug (_, v) -> [Env.singleton v t]
-    | Pair (v1, v2) ->
-      split_pair t
-      |> List.map (
-        fun (t1, t2) ->
-          let env1 = Env.singleton v1 t1 in
-          let env2 = Env.singleton v2 t2 in
-          Env.cap env1 env2
-      )
-    | Projection (Fst, v) -> [mk_times (cons t) any_node |> Env.singleton v]
-    | Projection (Snd, v) -> [mk_times any_node (cons t) |> Env.singleton v]
-    | Projection _ -> failwith "Not implemented"
-    | RecordUpdate _ -> failwith "Not implemented"
-    | App (v1, v2) ->
-      let t1 = Env.find v1 env in
-      (if backward then square_split t1 t else triangle_split t1 t)
-      |> List.map (
-        fun (t1, t2) ->
-          let env1 = Env.singleton v1 t1 in
-          let env2 = Env.singleton v2 t2 in
-          Env.cap env1 env2
-      )
-    | Ite (v, s, x1, x2) ->
-      let env1 = Env.singleton v s in
-      let env2 = Env.singleton v (neg s) in
-      let env1' = Env.singleton x1 t in
-      let env2' = Env.singleton x2 t in
-      [Env.cap env1 env1' ; Env.cap env2 env2']
-    | Lambda (Ast.ADomain s, _, _) when backward ->
-      if subtype (domain t) s then [Env.empty] else []
-    | Lambda (Ast.ADomain _, _, _) -> []
-    | Lambda _ -> failwith "Not implemented"
+let refine_a _ ~backward _ env a t =
+  begin match a with
+  | Const c when backward ->
+    if disjoint (Ast.const_to_typ c) t then [] else [Env.empty]
+  | Const c ->
+    if subtype (Ast.const_to_typ c) t then [Env.empty] else []
+  | Var v -> [Env.singleton v t]
+  | Debug (_, v) -> [Env.singleton v t]
+  | Pair (v1, v2) ->
+    split_pair t
+    |> List.map (
+      fun (t1, t2) ->
+        let env1 = Env.singleton v1 t1 in
+        let env2 = Env.singleton v2 t2 in
+        Env.cap env1 env2
+    )
+  | Projection (Fst, v) -> [mk_times (cons t) any_node |> Env.singleton v]
+  | Projection (Snd, v) -> [mk_times any_node (cons t) |> Env.singleton v]
+  | Projection _ -> failwith "Not implemented"
+  | RecordUpdate _ -> failwith "Not implemented"
+  | App (v1, v2) ->
+    let t1 = Env.find v1 env in
+    (if backward then square_split t1 t else triangle_split t1 t)
+    |> List.map (
+      fun (t1, t2) ->
+        let env1 = Env.singleton v1 t1 in
+        let env2 = Env.singleton v2 t2 in
+        Env.cap env1 env2
+    )
+  | Ite (v, s, x1, x2) ->
+    let env1 = Env.singleton v s in
+    let env2 = Env.singleton v (neg s) in
+    let env1' = Env.singleton x1 t in
+    let env2' = Env.singleton x2 t in
+    [Env.cap env1 env1' ; Env.cap env2 env2']
+  | Lambda (Ast.ADomain s, _, _) when backward ->
+    if subtype (domain t) s then [Env.empty] else []
+  | Lambda (Ast.ADomain _, _, _) -> []
+  | Lambda _ -> failwith "Not implemented"
   end
-  |> List.filter (fun env -> Env.is_bottom env |> not)
-
-and refine ~backward tenv env e t = (* TODO: Update with the new system... *)
-  if Env.is_bottom env then []
-  else begin
-    match e with
-    | EVar v -> refine_a [] ~backward tenv env (Var v) t
-    | Let (v, a, e) ->
-      let rm_v = if backward then (fun env -> env) else (fun env -> Env.rm v env) in
-      let env_nov = rm_v env in (* Shouldn't raise Env.EnvIsBottom because emptiness is checked before *)
-      refine ~backward tenv env e t
-      |> List.map (fun env' ->
-        if Env.mem v env'
-        then
-          let env_nov' = rm_v env' in (* Shouldn't raise Env.EnvIsBottom because refine only return non-empty envs *)
-          refine_a [] ~backward tenv (Env.cap env_nov env_nov') a (Env.find v env')
-          |> List.map (Env.cap env_nov')
-        else [env']
-      )
-      |> List.flatten
-      |> (fun envs -> (if backward then [] else 
-        refine_a [] ~backward tenv env_nov a empty
-        )@envs)
-  end
-  |> List.filter (fun env -> Env.is_bottom env |> not)
+  |> List.map (fun env' -> List.fold_left (fun acc v ->
+    if Env.mem v env then Env.strengthen v (Env.find v env) acc else acc)
+    env' (Env.domain env')) (* Inter *)
+  |> List.filter (fun env -> Env.is_bottom env |> not) (* Normalize *)
 
 let infer _ _ _ _ = failwith "TODO"
 and infer_a _ _ _ _ _ = failwith "TODO"

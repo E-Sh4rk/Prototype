790585b5d78e8b0a2f1ff76397218cf30aad197c
Mickael Laurent <esh4rk@gmail.com>
2020-12-29 22:02:12 +0100

    implement annotations.ml

--- src/annotations.ml
+++ src/annotations.ml
@@ -3,18 +3,29 @@ open Variable
 module VarAnnot = struct
   type t = (Env.t * Cduce.typ) list
   let empty = []
-  let splits _ _ = failwith "TODO"
-  let add_split _ _ _ = failwith "TODO"
+  let splits env ?(initial=Cduce.any) va =
+    List.filter (fun (env',_) -> Env.leq env env') va
+    |> List.map (fun (_,snd) -> Cduce.cap snd initial)
+    |> List.filter (fun t -> Cduce.is_empty t |> not)
+  let add_split env typ va = (env, typ)::va
 end
 
 module Annotations = struct
   type t = VarAnnot.t VarMap.t
   let empty = VarMap.empty
-  let splits _ _ _ = failwith "TODO"
-  let add_split _ _ _ _ = failwith "TODO"
 
-  let mem_var _ _ = failwith "TODO"
-  let add_var _ _ _ = failwith "TODO"
-  let remove_var _ _ = failwith "TODO"
-  let get_var _ _ = failwith "TODO"
+  let mem_var = VarMap.mem
+  let add_var = VarMap.add
+  let remove_var = VarMap.remove
+  let get_var = VarMap.find
+
+  let splits v env ?(initial=Cduce.any) annots =
+    if mem_var v annots
+    then get_var v annots |> VarAnnot.splits env ~initial
+    else [initial]
+  
+  let add_split v env typ annots =
+    let va = if mem_var v annots then get_var v annots else VarAnnot.empty in
+    let va = VarAnnot.add_split env typ va in
+    add_var v va annots
 end
--- src/annotations.mli
+++ src/annotations.mli
@@ -3,15 +3,15 @@ open Variable
 module VarAnnot : sig
   type t
   val empty : t
-  val splits : Env.t -> t -> Cduce.typ list
+  val splits : Env.t -> ?initial:Cduce.typ -> t -> Cduce.typ list
   val add_split : Env.t -> Cduce.typ -> t -> t
 end
 
 module Annotations : sig
   type t
   val empty : t
-  val splits : Variable.t -> Env.t -> t -> Cduce.typ list
-  val add_split : Variable.t ->Env.t ->  Cduce.typ -> t -> t
+  val splits : Variable.t -> Env.t -> ?initial:Cduce.typ -> t -> Cduce.typ list
+  val add_split : Variable.t -> Env.t ->  Cduce.typ -> t -> t
 
   val mem_var : Variable.t -> t -> bool
   val add_var : Variable.t -> VarAnnot.t -> t -> t

8a8ac5ba451411dc73bb1aaf95a4f69d8c81cef5
Mickael Laurent <esh4rk@gmail.com>
2021-07-03 14:48:31 +0200

    clean annotations

--- src/annotations.ml
+++ src/annotations.ml
@@ -1,4 +1,3 @@
-(*open Variable*)
 
 let partition_aux lst is_empty disjoint cap diff =
   let rec aux lst =
@@ -32,7 +31,7 @@ module VarAnnot = struct
   type t = (Env.t * Cduce.typ) list
   let empty = []
   let any = [Env.empty, Cduce.any]
-  let initial = empty (* NOTE: 'any' in the paper (more optimized with 'empty') *)
+  let initial = any
   let is_empty va = va = []
 
   let singleton env t = [(env, t)]
@@ -67,45 +66,3 @@ module VarAnnot = struct
   let pp fmt t =
     Format.fprintf fmt "[%i]" (List.length t)
 end
-
-(*module Annotations = struct
-  type t = VarAnnot.t VarMap.t
-  let empty = VarMap.empty
-
-  let mem_var = VarMap.mem
-  let add_var = VarMap.add
-  let remove_var = VarMap.remove
-  let get_var = VarMap.find
-
-  let is_undefined v t =
-    mem_var v t |> not
-
-  let is_empty v t =
-    if mem_var v t
-    then (get_var v t |> VarAnnot.is_empty)
-    else true
-
-  let restrict vs annots =
-    VarSet.fold (fun v acc ->
-      if mem_var v annots
-      then VarMap.add v (get_var v annots) acc
-      else acc
-    )
-    vs VarMap.empty
-
-  let splits v env annots =
-    if mem_var v annots
-    then get_var v annots |> VarAnnot.splits env
-    else []
-  
-  let add_split v env typ annots =
-    let va = if mem_var v annots then get_var v annots else VarAnnot.empty in
-    let va = VarAnnot.add_split env typ va in
-    add_var v va annots
-
-  let cup a1 a2 =
-    VarMap.union (fun _ va1 va2 -> Some (VarAnnot.cup va1 va2)) a1 a2
-
-  let union lst =
-    List.fold_left cup empty lst
-end*)
--- src/annotations.mli
+++ src/annotations.mli
@@ -1,4 +1,3 @@
-(*open Variable*)
 
 val partition : Cduce.typ -> Cduce.typ list -> Cduce.typ list
 val partition_for_full_domain : Cduce.typ list -> Cduce.typ list
@@ -18,20 +17,3 @@ module VarAnnot : sig
   val pp_filtered : string list -> Format.formatter -> t -> unit
   val pp : Format.formatter -> t -> unit
 end
-
-(*module Annotations : sig
-  type t
-  val empty : t
-  val is_undefined : Variable.t -> t -> bool
-  val is_empty : Variable.t -> t -> bool
-  val splits : Variable.t -> Env.t -> t -> Cduce.typ list
-  val add_split : Variable.t -> Env.t -> Cduce.typ -> t -> t
-  val cup : t -> t -> t
-  val union : t list -> t
-
-  val mem_var : Variable.t -> t -> bool
-  val add_var : Variable.t -> VarAnnot.t -> t -> t
-  val remove_var : Variable.t -> t -> t
-  val get_var : Variable.t -> t -> VarAnnot.t
-  val restrict : VarSet.t -> t -> t
-end*)

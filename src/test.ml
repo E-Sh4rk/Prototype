1996d943329b547b33b165459aaaa73ddaf5d74e
Beppe <beppe@irif.fr>
2021-07-06 19:17:16 +0200

    added implicit version of TH&F examples

--- src/test.ml
+++ src/test.ml
@@ -1,6 +1,37 @@
 let bool = <Bool>
 
-let test = if bool is True then true else false
+
+(*************************************************
+*                                                *
+*           IMPLICITLY ANNOTATED VERSIONS        *
+*                                                *
+**************************************************)   
+
+
+let lnot = fun a ->
+  if a is True then false else true
+
+let lor = fun a -> fun b ->
+  if a is False then if b is False then false else true else true
+
+let land = fun a -> fun b ->
+  if a is True then if b is False then false else true else false
+
+let test_1 = fun x -> fun y ->
+  if land (lor x (lnot x)) (lor (lnot y) y) then true else false
+
+let is_int = fun x -> if x is Int then true else false
+let is_bool = fun x -> if x is Bool then true else false
+
+
+
+
+(*************************************************
+*                                                *
+*           EXPLICITLY ANNOTATED VERSIONS        *
+*                                                *
+**************************************************)   
+
 
 debug let lnot = fun (a : Any) ->
   if a is True then false else true
@@ -276,8 +307,11 @@ let and_ = fun (x : Any) ->
 
 let and2_ = fun (x : (Any,Any)) -> if fst x is True then if snd x is True then fst x else false else false
 
+let and3_ = fun x ->
+            fun y -> if x is True then if y is True then y else false else false
+
 let or_ = fun (x : Any) ->
-fun (y:Any) -> if x is True then true else if y is True then true else false
+          fun (y:Any) -> if x is True then true else if y is True then true else false
 
 let not_ = fun (x : Any) -> if x is True then true else false
 let add1 = fun (Int -> Int) x -> magic
@@ -296,49 +330,97 @@ let f = fun ((Int | String) -> Int) x -> magic
 
 let g = fun ((Int, Int) -> Int) x -> magic
 
+
+(*************************************************
+*              Tobin & Felleisen                 *
+*     exampleX = EXPLICITLY ANNOTATED VERSIONS   *
+*     implictX = IMPLICITLY ANNOTATED VERSIONS   *
+*                                                *
+**************************************************)
+
+(*Interesting points:
+  - example2: we do not need the annotation, while TH&F yes
+  - example6: not typable with the annotation, but
+    if WE remove annotations becomes typable
+  - in examples 10 11 12 we do not have to assume that p is
+    a product the system deduces it alone
+  - same for the example 14. We do not have to assume that
+    the input is Int|String and extra is a pair. The system
+    finds it alone and it works for user defined and (currified or not)
+*)  
 (* Examples Tobin & Felleisen *)
 
 let example1 = fun (x:Any) ->
      if x is Int then add1 x else 0
 
 
+let implict1 = fun x ->
+     if x is Int then add1 x else 0
+
 let example2 = fun (x:String|Int) ->
   if x is Int then add1 x else strlen x
 
+let implict2 = fun x ->
+  if x is Int then add1 x else strlen x
 
 let example3 = fun (x: Any) ->
     if x is (Any \ False) then (x,x) else false
 
+let implict3 = fun x ->
+    if x is (Any \ False) then (x,x) else false
 
 let example4 = fun (x : Any) ->
    if or_ (is_int x) (is_string x) is True then x else 'A'
 
+let implict4 = fun x ->
+   if or_ (is_int x) (is_string x) is True then x else 'A'
 
-let example5 = fun (x : Any) -> fun (y : Any) ->
+let example5 = fun x -> fun y ->
    if and_ (is_int x) (is_string y) is True then
     add x (strlen y) else 0
 
+let implict5 = fun x -> fun y ->
+   if and_ (is_int x) (is_string y) is True then
+    add x (strlen y) else 0
 
 let example6 = fun (x : Int|String) -> fun (y : Any) ->
-if and_ (is_int x) (is_string y) is True then
-  add  x (strlen y) else strlen x
+   if and_ (is_int x) (is_string y) is True then
+    add  x (strlen y) else strlen x
+
+
+let implict6 = fun x -> fun y ->
+   if and_ (is_int x) (is_string y) is True then
+    add  x (strlen y) else strlen x
 
 
 let example7 = fun (x : Any) -> fun (y : Any) ->
-if  (if (is_int x) is True then (is_string y) else false) is True then
-  add x (strlen y) else 0
+   if  (if (is_int x) is True then (is_string y) else false) is True then
+    add x (strlen y) else 0
 
+let implict7 = fun x -> fun y ->
+   if  (if (is_int x) is True then (is_string y) else false) is True then
+    add x (strlen y) else 0
 
 let example8 = fun (x : Any) -> if or_ (is_int x) (is_string x) is True then true else false
 
+let implict8 = fun x -> if or_ (is_int x) (is_string x) is True then true else false
+
 
 let example9 = fun (x : Any) ->
-if
-  (if is_int x is True then is_int x else is_string x)
-  is True then  f x else 0
+  if
+     (if is_int x is True then is_int x else is_string x)
+     is True then  f x else 0
+
+let implict9 = fun (x : Any) ->
+  if
+     (if is_int x is True then is_int x else is_string x)
+     is True then  f x else 0
 
 
 let example10 = fun (p : (Any,Any)) ->
+   if is_int (fst p) is True then add1 (fst p) else 7
+
+let implict10 = fun p ->
    if is_int (fst p) is True then add1 (fst p) else 7
 
 
@@ -346,8 +428,13 @@ let example11 = fun (p : (Any, Any)) ->
    if and_ (is_int (fst p)) (is_int (snd p)) is True then g p else no
 
 
+let implict11 = fun p ->
+   if and_ (is_int (fst p)) (is_int (snd p)) is True then g p else no
+
+
 let example12 = fun (p : (Any, Any)) -> if is_int (fst p) is True then true else false
 
+let implict12 = fun p -> if is_int (fst p) is True then true else false
 
 let example13 =
     fun (x : Any) ->
@@ -356,22 +443,57 @@ let example13 =
        else if is_int x is True then 2
        else 3
 
-let example14_alt = fun (input : Int | String) -> fun (extra : (Any, Any)) ->
-    if and2_ ((is_int input),(is_int (fst extra)))  is True then
-       add input (fst extra)
-    else if (is_int input,is_int (fst extra)) is (Any,True) then add (strlen input) (fst extra)
-    else 0
+let implict13 =
+    fun x ->
+      fun y ->
+       if and_ (is_int x) (is_string y) is True then 1
+       else if is_int x is True then 2
+       else 3
 
-let example14 = fun (input : Int|String) ->
-  fun (extra : (Any, Any)) ->
+
+let example14 = fun (input : Int|String) -> fun (extra : (Any, Any)) ->
     if and2_(is_int input , is_int(fst extra)) is True then
         add input (fst extra)
     else if is_int(fst extra) is True then
         add (strlen input) (fst extra)
     else 0
 
-let example14_curry = fun (input : Int|String) ->
+
+let implct14a = fun (input : Int|String) ->
+  fun extra ->
+    if and2_(is_int input , is_int(fst extra)) is True then
+        add input (fst extra)
+    else if is_int(fst extra) is True then
+        add (strlen input) (fst extra)
+    else 0
+
+let implct14b = fun input ->
+  fun extra ->
+    if and2_(is_int input , is_int(fst extra)) is True then
+        add input (fst extra)
+    else if is_int(fst extra) is True then
+        add (strlen input) (fst extra)
+    else 0
+
+let curried14 = fun (input : Int|String) ->
   fun (extra : (Any, Any)) ->
+    if and_ (is_int input) (is_int(fst extra)) is True then
+        add input (fst extra)
+    else if is_int (fst extra) is True then
+        add (strlen input) (fst extra)
+    else 0
+
+let currid14a = fun (input : Int|String) ->
+  fun extra ->
+    if and_ (is_int input) (is_int(fst extra)) is True then
+        add input (fst extra)
+    else if is_int (fst extra) is True then
+        add (strlen input) (fst extra)
+    else 0
+
+
+let currid14b = fun input ->
+  fun extra ->
     if and_ (is_int input) (is_int(fst extra)) is True then
         add input (fst extra)
     else if is_int (fst extra) is True then
@@ -409,7 +531,15 @@ let example_new2 =
 fun (a : (Int -> (Int|Bool)) | ( Int, (Int|Bool))) ->
 fun n ->
   if a is (Int,Int) then ((fst a)=(snd a))
-  else if a is (Any,Any) then ((snd a) + (n = 42))
+  else if a is (Any,Any) then ((snd a)+(n = 42))
+  else if (a n) is Int then ((a n) =  42)
+  else (a n)
+
+let example_new3 =
+fun (a : (Int -> (Int|Bool)) | ( Int, (Int|Bool))) ->
+fun n ->
+  if a is (Int,Int) then ((fst a)=(snd a))
+  else if a is (Any,Any) then (and_ (snd a) (n = 42))
   else if (a n) is Int then ((a n) =  42)
   else (a n)
 
@@ -461,6 +591,9 @@ bind aux2 = aux1 42 in
 bind aux3 = aux2 + 1
 *)
 
+
+let test = fun x -> x + 1
+let test = fun(x:Any) -> x + 1
 
 let negate = fun f -> (fun x -> lnot (f x))
 

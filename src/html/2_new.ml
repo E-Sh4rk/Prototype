3aa53a694a59f396ad3b7287cbc38078f6a0ba6b
Kim Nguyễn <kim.nguyen@lri.fr>
2021-11-10 11:00:16 +0100

    Add the revised example 2 and fst/snd to the grammar.

---
+++ src/html/2_new.ml
@@ -0,0 +1,17 @@
+(* Code 2 from the final version *)
+
+type Falsy = False | "" | 0
+type Truthy = ~Falsy
+
+let not_ = fun x ->
+  if x is Truthy then false else true
+
+let to_Bool = fun x -> not_ (not_ x)
+
+
+let and_ = fun x -> fun y ->
+  if x is Truthy then to_Bool y else false
+
+
+let or_ = fun x -> fun y ->
+  not_ (and_ (not_ x) (not_ y))

8401773834f310f39b1f3778a9252f0b451c5ba1
Mickael Laurent <mlaurent@irif.fr>
2022-02-25 11:22:15 +0100

    refactor

--- src/checker.ml
+++ src/checker.ml
@@ -145,19 +145,19 @@ let refine_a tenv env a t =
     |> List.filter_map (
       fun (t1, t2) ->
         env |>
-        option_chain [Env_refinement.refine_existing v1 t1 ; Env_refinement.refine_existing v2 t2]
+        option_chain [Env_refinement.refine v1 t1 ; Env_refinement.refine v2 t2]
     )
-  | Projection (Fst, v) -> [Env_refinement.refine_existing v (mk_times (cons t) any_node) env] |> filter_options
-  | Projection (Snd, v) -> [Env_refinement.refine_existing v (mk_times any_node (cons t)) env] |> filter_options
+  | Projection (Fst, v) -> [Env_refinement.refine v (mk_times (cons t) any_node) env] |> filter_options
+  | Projection (Snd, v) -> [Env_refinement.refine v (mk_times any_node (cons t)) env] |> filter_options
   | Projection (Field label, v) ->
-    [Env_refinement.refine_existing v (mk_record true [label, cons t]) env] |> filter_options
+    [Env_refinement.refine v (mk_record true [label, cons t]) env] |> filter_options
   | RecordUpdate (v, label, None) ->
     let t = cap_o t (record_any_without label) in
     split_record t
     |> List.filter_map (
       fun ti ->
           let ti = remove_field_info ti label in
-          Env_refinement.refine_existing v ti env
+          Env_refinement.refine v ti env
     )
   | RecordUpdate (v, label, Some x) ->
     split_record t
@@ -166,7 +166,7 @@ let refine_a tenv env a t =
         let field_type = get_field_assuming_not_absent ti label in
         let ti = remove_field_info ti label in
         env |>
-        option_chain [Env_refinement.refine_existing v ti ; Env_refinement.refine_existing x field_type]
+        option_chain [Env_refinement.refine v ti ; Env_refinement.refine x field_type]
       )
   | App (v1, v2) ->
     let t1 = Env_refinement.find v1 env in
@@ -174,17 +174,17 @@ let refine_a tenv env a t =
     |> List.filter_map (
       fun (t1, t2) ->
         env |>
-        option_chain [Env_refinement.refine_existing v1 t1 ; Env_refinement.refine_existing v2 t2]
+        option_chain [Env_refinement.refine v1 t1 ; Env_refinement.refine v2 t2]
     )
   | Ite (v, s, x1, x2) ->
-    [ env |> option_chain [Env_refinement.refine_existing v s       ; Env_refinement.refine_existing x1 t] ;
-      env |> option_chain [Env_refinement.refine_existing v (neg s) ; Env_refinement.refine_existing x2 t] ]
+    [ env |> option_chain [Env_refinement.refine v s       ; Env_refinement.refine x1 t] ;
+      env |> option_chain [Env_refinement.refine v (neg s) ; Env_refinement.refine x2 t] ]
     |> filter_options
   | Lambda _ ->
     if disjoint arrow_any t then [] else [env]
   | Let (v1, v2) ->
     [ env |>
-    option_chain [Env_refinement.refine_existing v1 any ; Env_refinement.refine_existing v2 t]]
+    option_chain [Env_refinement.refine v1 any ; Env_refinement.refine v2 t]]
     |> filter_options
 
 let propagate tenv x a gammas =

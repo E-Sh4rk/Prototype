76cbf64908f0bd8a003e521dab5e080080af9e48
Mickael Laurent <mlaurent@irif.fr>
2022-07-08 10:54:46 -0400

    update checker to use new refinable_env module

--- src/checker.mli
+++ src/checker.mli
@@ -9,7 +9,7 @@ val typeof :  type_env -> Env.t -> AnnotMono.e -> e -> typ
 val typeof_a : Position.t list -> type_env -> Env.t -> AnnotMono.a -> a -> typ
 
 val refine_a : sufficient:bool -> type_env ->
-    Env_refinement.t -> a -> typ -> typ -> Env_refinement.t list
+    Refinable_env.t -> a -> typ -> typ -> Refinable_env.t list
 
 val infer : type_env -> Env.t -> e -> e * AnnotMono.e
 val typeof_simple : type_env -> Env.t -> e -> typ

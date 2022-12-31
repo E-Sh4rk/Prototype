16cb049f94bec34f651a381b92b6e98d1ba52e4f
Mickael Laurent <esh4rk@gmail.com>
2022-12-31 19:24:00 +0100

    refactor

--- src/parsing/IO.ml
+++ src/parsing/IO.ml
@@ -5,32 +5,31 @@ let print_position outx lexbuf =
   Format.fprintf outx "%s:%d:%d" pos.pos_fname
     pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
 
+let parse_with_errors parser buf =
+  try parser Lexer.token buf with
+  | Lexer.LexerError msg ->
+    raise (Ast.LexicalError (Format.asprintf "%a" print_position buf, msg))
+  | Parser.Error ->
+    raise (Ast.SyntaxError (Format.asprintf "%a" print_position buf, "Syntax error"))
+
 let parse_expr_file source_filename =
   let cin = open_in source_filename in
-  let buf = Lexing.from_channel cin in
+  let buf = from_channel cin in
   buf.lex_curr_p <- { buf.lex_curr_p with  pos_fname = source_filename };
-  Parser.unique_term Lexer.token buf
+  parse_with_errors Parser.unique_term buf
 
 let parse_expr_string str =
-  let buf = Lexing.from_string str in
+  let buf = from_string str in
   buf.lex_curr_p <- { buf.lex_curr_p with  pos_fname = "_" };
-  Parser.unique_term Lexer.token buf
+  parse_with_errors Parser.unique_term buf
 
 let parse_program_file source_filename =
   let cin = open_in source_filename in
-  let buf = Lexing.from_channel cin in
+  let buf = from_channel cin in
   buf.lex_curr_p <- { buf.lex_curr_p with  pos_fname = source_filename };
-  try Parser.program Lexer.token buf with
-  | Lexer.LexerError msg ->
-    raise (Ast.LexicalError (Format.asprintf "%a" print_position buf, msg))
-  | Parser.Error ->
-    raise (Ast.SyntaxError (Format.asprintf "%a" print_position buf, "Syntax error"))
+  parse_with_errors Parser.program buf
 
 let parse_program_string str =
-  let buf = Lexing.from_string str in
+  let buf = from_string str in
   buf.lex_curr_p <- { buf.lex_curr_p with  pos_fname = "_" };
-  try Parser.program Lexer.token buf with
-  | Lexer.LexerError msg ->
-    raise (Ast.LexicalError (Format.asprintf "%a" print_position buf, msg))
-  | Parser.Error ->
-    raise (Ast.SyntaxError (Format.asprintf "%a" print_position buf, "Syntax error"))
+  parse_with_errors Parser.program buf
